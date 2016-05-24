88c12e78b80d97fff51c831f38d2c80c42f1df13
anand raman <IO85@global.Tesco.org>
2016-05-24 08:49:14 +0100

    dont like nested if; but acceptable complexity for the tests. may refactor when more use cases are added

--- src/main/java/cleancode/kata/orderamount/OrderAmount.java
+++ src/main/java/cleancode/kata/orderamount/OrderAmount.java
@@ -2,11 +2,11 @@ package cleancode.kata.orderamount;
 
 public class OrderAmount {
 
-  private String[] units = {"", "one", "two", "three", "four", "five", "six", "seven", "eight",
-      "nine", "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen",
-      "eighteen", "nineteen", "twenty"};
+  private String[] unitsAndTeenSpellings = {"", "one", "two", "three", "four", "five", "six",
+      "seven", "eight", "nine", "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen",
+      "sixteen", "seventeen", "eighteen", "nineteen"};
 
-  private String[] tens =
+  private String[] tensSpellings =
       {"", "", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"};
 
   private int amount;
@@ -16,11 +16,15 @@ public class OrderAmount {
   }
 
   public String asText() {
-    if (amount <= 20) {
-      return units[amount];
+    if (amount < 20) {
+      return unitsAndTeenSpellings[amount];
     } else {
       int tenCount = amount / 10;
-      return tens[tenCount] + " " + units[amount - (tenCount * 10)];
+      if (amount % 10 != 0) {
+        return tensSpellings[tenCount] + " " + unitsAndTeenSpellings[amount - (tenCount * 10)];
+      } else {
+        return tensSpellings[tenCount];
+      }
     }
   }
 
