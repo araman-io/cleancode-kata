c2250b5d6d1a093bd2db9f0db2b476381c0628d5
anand raman <IO85@global.Tesco.org>
2016-05-25 07:20:03 +0100

    a little more refactoring in tens speller; now can move up the method so other spellers can use

--- src/main/java/cleancode/kata/orderamount/TensSpeller.java
+++ src/main/java/cleancode/kata/orderamount/TensSpeller.java
@@ -2,17 +2,44 @@ package cleancode.kata.orderamount;
 
 public class TensSpeller implements Speller {
 
+  private static final UnitAndTeensSpeller NEXT_SPELLER = new UnitAndTeensSpeller();
+
   private String[] tensSpellings =
       {"", "ten", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"};
 
   @Override
   public String spell(int number) {
-    int quantum = number / 10;
-    String result = "";
-    result = (quantum > 1) ? tensSpellings[quantum] : "";
-    String nextFragment = new UnitAndTeensSpeller().spell(number - (quantum * 10));
-    result += nextFragment.isEmpty() ? "" : String.format(" %s",nextFragment);
-    return result;
+    String thisFragment = "";
+    String nextFragment = "";
+    int quantum = getQuantum(number);
+    thisFragment = (quantum > 1) ? tensSpellings[quantum] : "";
+    nextFragment = next(number, quantum);
+    return mergeFragments(thisFragment, nextFragment);
+  }
+
+  protected String next(int number, int quantum) {
+    String nextFragment = NEXT_SPELLER.spell(number - (quantum * getPlaceValue()));
+    return nextFragment;
+  }
+
+  protected int getQuantum(int number) {
+    int quantum = number / getPlaceValue();
+    return quantum;
+  }
+
+  protected int getPlaceValue() {
+    return 10;
+  }
+
+  protected String mergeFragments(String first, String second) {
+    String merged = "";
+    if (first.isEmpty()) {
+      merged = second.isEmpty() ? "" : second;
+    }
+    if (!first.isEmpty()) {
+      merged = second.isEmpty() ? first : String.format("%s %s", first, second);
+    }
+    return merged;
   }
 
 }
