c21b6951591be2da2d815f3734394af8624512bf
anand raman <IO85@global.Tesco.org>
2016-06-01 18:28:15 +0100

    introduced an enum for sku; a failing test case exists

--- src/main/java/cleancode/kata/checkout/Checkout.java
+++ src/main/java/cleancode/kata/checkout/Checkout.java
@@ -1,15 +1,22 @@
 package cleancode.kata.checkout;
 
+import static cleancode.kata.checkout.Sku.A;
+import static cleancode.kata.checkout.Sku.valueOf;
+
 public class Checkout {
 
-  private String product;
+  private Sku product;
 
   public void scan(String product) {
-    this.product = product;
+    this.scan(valueOf(product));;
   }
 
+  public void scan(Sku product) {
+    this.product = product;
+  }
+  
   public int total() {
-    return product.equals("A") ? 50 : 30;
+    return product.equals(A) ? 50 : 30;
   }
 
 }
--- src/test/java/cleancode/kata/checkout/CheckoutShould.java
+++ src/test/java/cleancode/kata/checkout/CheckoutShould.java
@@ -7,18 +7,28 @@ import org.junit.Test;
 import static org.junit.Assert.assertThat;
 
 public class CheckoutShould {
-  
+
   @Test
   public void price_1A_as_50() throws Exception {
     Checkout checkout = new Checkout();
     checkout.scan("A");
     assertThat(checkout.total(), is(50));
   }
-  
+
   @Test
   public void price_1B_as_30() throws Exception {
     Checkout checkout = new Checkout();
     checkout.scan("B");
     assertThat(checkout.total(), is(30));
   }
+
+
+  @Test
+  public void price_1A_1B_as_80() throws Exception {
+    Checkout checkout = new Checkout();
+    checkout.scan("A");
+    checkout.scan("B");
+    assertThat(checkout.total(), is(80));
   }
+
+}
