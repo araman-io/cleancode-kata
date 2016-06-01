aaa570d432f8ea66ad51b8f8618a4e350bfcc0b2
anand raman <IO85@global.Tesco.org>
2016-06-01 18:30:46 +0100

    loop through and add unit prices

--- src/main/java/cleancode/kata/checkout/Checkout.java
+++ src/main/java/cleancode/kata/checkout/Checkout.java
@@ -2,20 +2,23 @@ package cleancode.kata.checkout;
 
 import static cleancode.kata.checkout.Sku.valueOf;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Checkout {
 
-  private Sku product;
+  private List<Sku> products = new ArrayList<>();
 
   public void scan(String product) {
     scan(valueOf(product));;
   }
 
   public void scan(Sku product) {
-    this.product = product;
+    products.add(product);
   }
-  
+
   public int total() {
-    return product.unitPrice();
+    return products.stream().mapToInt(Sku::unitPrice).sum();
   }
 
 }
