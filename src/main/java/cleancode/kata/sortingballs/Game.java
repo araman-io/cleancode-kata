a71b5ac63f7a89177ffa47ef22123e3eeb4c75d2
Raman <IO85@global.Tesco.org>
2016-09-27 23:23:27 +0100

    replaced collections.sort with an inefficient bubble sort

--- src/main/java/cleancode/kata/sortingballs/Game.java
+++ src/main/java/cleancode/kata/sortingballs/Game.java
@@ -1,17 +1,30 @@
 package cleancode.kata.sortingballs;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
-import static java.util.Collections.sort;
+import static java.util.stream.IntStream.range;
 
 public class Game {
 
-  List<Draw> allDraws = new ArrayList<>();
+  Set<Draw> allDraws = new HashSet<>();
 
   public List<Draw> result() {
-    sort(allDraws);
-    return allDraws;
+    List<Draw> sortedDraws = new ArrayList<>();
+
+    range(0, allDraws.size()).forEach(index -> {
+      Draw nextElement = null;
+      for (Draw d : allDraws) {
+        if (nextElement == null || nextElement.compareTo(d) > 0) {
+          nextElement = d;
+        }
+      }
+      sortedDraws.add(nextElement);
+      allDraws.remove(nextElement);
+    });
+    return sortedDraws;
   }
 
   public void draw(Draw ball) {
--- src/test/java/cleancode/kata/sortingballs/GameShould.java
+++ src/test/java/cleancode/kata/sortingballs/GameShould.java
@@ -13,55 +13,69 @@ import static java.util.Arrays.asList;
 import static org.junit.Assert.assertThat;
 
 public class GameShould {
-  
+
   @Test
   public void returnNoBallsWhenCreated() {
     Game game = new Game();
     List<Draw> emptyList = new ArrayList<>();
     assertThat(game.result(), is(emptyList));
   }
-  
+
   @Test
   public void returnOneBallWhenOnlyOneBallHasBeenDrawn() {
     Game game = new Game();
     game.draw(of("57"));
-    
+
     List<Draw> expected = asList(of("57"));
-    
+
     assertThat(game.result(), is(expected));
   }
-  
+
   @Test
   public void returnBallsInTheSameOrderWhen2BallsAreDrawnInAscendingOrder() {
     Game game = new Game();
     game.draw(of("49"));
     game.draw(of("57"));
-    
+
     List<Draw> expected = asList(of("49"), of("57"));
-    
+
     assertThat(game.result(), is(expected));
   }
-  
+
   @Test
   public void returnBallsInAscendingOrderWhen2BallsAreDrawnInDescendingOrder() {
     Game game = new Game();
     game.draw(of("45"));
     game.draw(of("24"));
-    
+
     List<Draw> expected = asList(of("24"), of("45"));
-    
+
     assertThat(game.result(), is(expected));
   }
-  
+
   @Test
   public void returnBallsInAscendingOrderWhen3BallsAreDrawnInDescendingOrder() {
     Game game = new Game();
     game.draw(of("45"));
     game.draw(of("30"));
     game.draw(of("10"));
-    
+
     List<Draw> expected = asList(of("10"), of("30"), of("45"));
-    
+
+    assertThat(game.result(), is(expected));
+  }
+
+  @Test
+  public void returnBallsInAscendingOrderWhenBallsAreDrawnInMixedOrder() {
+    Game game = new Game();
+    game.draw(of("45"));
+    game.draw(of("53"));
+    game.draw(of("30"));
+    game.draw(of("10"));
+    game.draw(of("59"));
+
+    List<Draw> expected = asList(of("10"), of("30"), of("45"), of("53"), of("59"));
+
     assertThat(game.result(), is(expected));
   }
 
