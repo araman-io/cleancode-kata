06076fedd6b03563ac19ab484ed33703a80b1602
Raman <IO85@global.Tesco.org>
2016-12-20 06:31:10 +0000

    2 simple test cases

---
+++ src/main/java/cleancode/kata/rockpaperscissor/RockPaperScissors.java
@@ -0,0 +1,9 @@
+package cleancode.kata.rockpaperscissor;
+
+public class RockPaperScissors {
+
+  public static String determineWinner(String string, String string2) {
+    return string2;
+  }
+
+}
---
+++ src/test/java/cleancode/kata/rockpaperscissor/RockPaperScissorsTest.java
@@ -0,0 +1,19 @@
+package cleancode.kata.rockpaperscissor;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class RockPaperScissorsTest {
+
+  @Test
+  public void rockWinsOverScissors() {
+    assertEquals("ROCK", RockPaperScissors.determineWinner("SCISSORS", "ROCK"));
+  }
+
+  @Test
+  public void scissorsWinsOverPins() {
+    assertEquals("SCISSORS", RockPaperScissors.determineWinner("PAPER", "SCISSORS"));
+  }
+
+}
